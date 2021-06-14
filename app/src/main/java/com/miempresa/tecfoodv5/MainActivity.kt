e87aa7235b787588e4abc9a967b109182dcc2887
David Alejo <kaisel-xd@hotmail.com>
2021-06-14 11:19:29 -0500

    modificacion de prueba2

--- app/src/main/java/com/miempresa/tecfoodv5/MainActivity.kt
+++ app/src/main/java/com/miempresa/tecfoodv5/MainActivity.kt
@@ -72,6 +72,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
             // opciones menu lateral
             R.id.invitacion -> {
+                // modificado Toast.makeText(this, "elegiste menu invitacion", Toast.LENGTH_LONG).show()
                 Toast.makeText(this, "elegiste menu invitacion", Toast.LENGTH_LONG).show()
             }
             R.id.premiun -> {
