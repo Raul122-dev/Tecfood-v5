0f4acafeea872d7fd7f2c8856036c3c1e31d8959
unknown <diego.pamo@tecsup.edu.pe>
2021-06-15 13:18:41 -0500

    Login y Regitro

---
+++ app/src/main/java/com/miempresa/tecfoodv5/Activitys/LoginRegister/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.miempresa.tecfoodv5.Activitys.LoginRegister
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miempresa.tecfoodv5.MainActivity
+import com.miempresa.tecfoodv5.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        txt_login_invitado.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        login_phone.setOnClickListener(){
+            val intent = Intent(this, RegisterPhoneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/miempresa/tecfoodv5/Activitys/LoginRegister/RegisterPhoneActivity.kt
@@ -0,0 +1,12 @@
+package com.miempresa.tecfoodv5.Activitys.LoginRegister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miempresa.tecfoodv5.R
+
+class RegisterPhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_phone)
+    }
+}
